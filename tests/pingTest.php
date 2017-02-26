d65e0722bf2587c76ce5f3393a37062938bc4fc2
bimal <gist@bimal.org.np>
2017-02-26 16:05:37 -0700

    requestbin

--- tests/pingTest.php
+++ tests/pingTest.php
@@ -3,17 +3,39 @@ use PHPUnit\Framework\TestCase;
 
 class pingTest extends TestCase
 {
+	private $relay;
+
 	public function setup()
 	{
+		$this->relay = new relay();
 	}
 
 	public function testPing()
 	{
-		// query
-		// response is pong
-		$relay = new relay();
-		$actual = $relay->fetch("http://localhost/angular/public_html/api/ping.php");
+		$pingpongurl = "http://api.example.com/ping.php";
+		$actual = $this->relay->fetch($pingpongurl);
 		$expect = "pong";
+
 		$this->assertEquals($actual, $expect);
 	}
+
+	/**
+	 * @see http://requestb.in/{$id}?inspect
+	 */ 
+	public function testRequestBin()
+	{
+		$id = "0000000";
+
+		$_GET = array("name" => "pong");
+		$_POST = array("name" => "ping");
+
+		$actual = $this->relay->fetch("http://requestb.in/{$id}");
+
+		$expect = "ok";
+
+		$this->assertEquals($actual, $expect);
+	}
+	
+	// address
+	// server time
 }
